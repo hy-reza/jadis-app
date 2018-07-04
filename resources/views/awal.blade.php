cb4b2c826ec22bc89b3f1c304594c77928739ced
Adam Novrisal <damsnov20@gmail.com>
2018-07-04 11:43:47 +0700

    ganti nama univ

--- resources/views/awal.blade.php
+++ resources/views/awal.blade.php
@@ -71,7 +71,7 @@
 						<div class="banner-content text-center">
 							<p class="text-uppercase text-white">We work hard, we result perfect</p>
 							<h1 class="text-uppercase text-white">Kartu Rencana Studi</h1>
-							<h1 class="text-uppercase text-white">Universitas Malang Kuçeçwara</h1>
+							<h1 class="text-uppercase text-white">Universitas Swasta Malang</h1>
 							<!-- <a href="#" class="primary-btn banner-btn">Register</a> -->
 						</div>
 					</div>
